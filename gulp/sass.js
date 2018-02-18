bf78a8494a9245ad1f5a37f0869feea681f99a41
Cristina <kylie_212@hotmail.com>
2018-02-18 18:03:12 +0100

    added sass globbing

--- gulp/sass.js
+++ gulp/sass.js
@@ -1,9 +1,13 @@
 module.exports = function (gulp, $) {
 
     const sass = require('gulp-sass');
+    const sassGlob = require('gulp-sass-glob');
+
 
     gulp.task('sass', function () {
-      return gulp.src('./src/**/*.scss')
+     // get only the first level files, as globbing task will pack all css in the first level files
+      return gulp.src('./src/*.scss')
+        .pipe(sassGlob())
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./dist/css'));
     });
