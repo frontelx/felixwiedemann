e04aa2bd7a2382e9addb481df1803797c6f5be60
Simon Koch <agorilla@me.com>
2018-03-04 22:11:18 +0100

    Removed obsolete line from gulp sass task

--- gulp/sass.js
+++ gulp/sass.js
@@ -3,7 +3,6 @@ module.exports = function (gulp, $) {
     const sass = require('gulp-sass');
     const sassGlob = require('gulp-sass-glob');
 
-
     gulp.task('sass', function () {
         // get only the first level files, as globbing task will pack all css in the first level files
         return gulp.src('./src/*.scss')
