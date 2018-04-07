c7ffc9e01ebbdce5f0d207886e7475d9ff3f86c9
Simon Koch <agorilla@me.com>
2018-04-07 18:13:22 +0200

    Added dynamic name in header

--- src/modules/m-header/m-header.vue
+++ src/modules/m-header/m-header.vue
@@ -1,6 +1,6 @@
 <template>
     <header class="m-header">
-        <h1>Felix Wiedemann</h1>
+        <h2>{{ $root.title.name }}</h2>
         <p-navigation></p-navigation>
     </header>
 </template>
