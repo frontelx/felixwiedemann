7610e317771b22952e81a05551eb8681350375e8
Cristina Coves Mora <kylie_212@hotmail.com>
2018-04-15 21:21:29 +0200

    [+TASK] added HTML/CSS for 'm-player'

--- src/modules/m-player/m-player.vue
+++ src/modules/m-player/m-player.vue
@@ -1,10 +1,27 @@
 <template>
     <div class="m-player">
-        Clip: {{ $route.params.clip }}
 
-        <iframe :src="`https://player.vimeo.com/video/${clip.video}?autoplay=1`" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen class="video-player-content"></iframe>
+        <div class="m-player__video">
+            <iframe :src="`https://player.vimeo.com/video/${clip.video}?autoplay=1`" frameborder="0"
+                    webkitallowfullscreen mozallowfullscreen allowfullscreen class="m-player__iframe"></iframe>
+        </div>
+
+        <div class="m-player__txt p-txt-rte">
+            <h2 class="m-player__title">CITY OF TINY LIGHTS / Feature Film - Trailer</h2>
+            <p class="m-player__descrp">Dir. Pete Travis / Prod. Rebecca O Brien, Ado Yoshizaki Cassuto / BBC Films, Sixteen Films</p>
+            <p class="m-player__descrp">Lorem ipsum dolor sit amet ipsum dolor sit amet.</p>
+        </div>
 
     </div>
+
+    <!--<ul class="framegrabs">
+        <li class="framegrabs__item">
+            <picture>
+                <source :srcset="`/content/clips/${clip.image}.jpg`" media="(min-width: 750px)">
+                <img class="p-img-block" :src="`/content/clips/${clip.image}-mobile.jpg`" alt="" :title="clip.title">
+            </picture>
+        </li>
+    </ul>-->
 </template>
 
 
