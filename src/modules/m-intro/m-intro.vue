4e0fc7523ae408dbb5c0bff499af9a0ecb82997c
Simon Koch <agorilla@me.com>
2018-04-22 23:54:30 +0200

    Added intro image transition

--- src/modules/m-intro/m-intro.vue
+++ src/modules/m-intro/m-intro.vue
@@ -2,8 +2,32 @@
     <div class="m-intro">
 
         <picture>
-            <source :srcset="`/content/intro/images/${currentImage}.jpg`" media="(min-width: 750px)">
-            <img class="m-intro__img" :src="`/content/intro/images/${currentImage}-mobile.jpg`" alt="" title="">
+            <source :srcset="currentImage" media="(min-width: 750px)">
+            <img
+                :src="currentImage"
+                class="m-intro__img"
+                alt="">
+        </picture>
+
+        <picture v-if="this.images.length > 1">
+            <source :srcset="nextImage" media="(min-width: 750px)">
+            <img
+                :src="nextImage"
+                :style="`
+                    transition-property: ${nextImageTransitionProperty};
+                    transition-duration: ${transitionDuration}s;
+                    opacity: ${nextImageOpacity}`"
+                class="m-intro__img m-intro__img--next"
+                alt=""
+                @transitionend="triggerImageChange()">
+        </picture>
+
+        <picture v-if="!this.hasPreloaded">
+            <source :srcset="preloadImage" media="(min-width: 750px)">
+            <img
+                :src="preloadImage"
+                class="m-intro__img m-intro__img--preload"
+                alt="">
         </picture>
 
         <div class="m-intro__txt-wp">
@@ -20,16 +44,88 @@
     import imagesData from '../../../content/intro/images.json';
     import Services from '../../generic/services';
 
-    // Randomizes image order
-    const shuffledImages = Services.shuffleArray(imagesData);
-
     export default {
+
+        props: {
+            transitionDelay: {
+                type: Number,
+                default: 3,
+            },
+            transitionDuration: {
+                type: Number,
+                default: 3,
+            },
+            transitionProperty: {
+                type: String,
+                default: 'opacity',
+            },
+        },
+
         data() {
             return {
-                images: shuffledImages,
-                currentImage: shuffledImages[0].image,
-                nextImage: shuffledImages[1].image,
+                images: Services.shuffleArray(imagesData),
+                imageCounter: 0,
+                currentImage: '',
+                nextImage: '',
+                nextImageOpacity: 0,
+                nextImageTransitionProperty: this.transitionProperty,
+                preloadImage: '',
+                hasPreloaded: false,
             }
+        },
+
+        created() {
+            if (this.images.length > 0) {
+                this.changeImage();
+            }
+
+        },
+
+        mounted() {
+            // Triggers first image transition
+            this.startTransition();
+        },
+
+        methods: {
+            triggerImageChange() {
+                this.stopTransition();
+                this.changeOpacity(0);
+                this.changeImage();
+                this.startTransition();
+            },
+
+            changeImage() {
+                // Set next image to current image after transition end
+                this.currentImage = this.images[this.imageCounter].image;
+
+                // Update image counter to next image or first image if no more available
+                this.imageCounter = this.imageCounter < this.images.length - 1 ? this.imageCounter + 1 : 0;
+                this.nextImage = this.images[this.imageCounter].image;
+
+                // Set next image to preload or remove preloading if no more images available
+                if (!this.hasPreloaded && this.imageCounter + 1 < this.images.length) {
+                   this.preloadImage = this.images[this.imageCounter + 1].image;
+                } else {
+                    this.hasPreloaded = true;
+                }
+            },
+
+            changeOpacity(value) {
+                setTimeout(() => {
+                    this.nextImageOpacity = value;
+                }, 1);
+            },
+
+            stopTransition() {
+                this.nextImageTransitionProperty = 'none';
+            },
+
+            startTransition() {
+                setTimeout(() => {
+                    this.nextImageTransitionProperty = this.transitionProperty;
+                    this.changeOpacity(1);
+                }, this.transitionDelay * 1000);
+            },
         },
     }
 </script>
