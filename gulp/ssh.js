78ec09d5b95ede550a9d8a2998369d4980939ec9
Simon Koch <agorilla@me.com>
2018-04-29 20:55:04 +0200

    Added SSH upload gulp task

---
+++ gulp/ssh.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const GulpSSH = require('gulp-ssh');
+
+const remotePath = './webseiten/felixwiedemann-2018';
+const serverConfigPath = 'serverconfig.json';
+
+const serverConfig = fs.existsSync(serverConfigPath) ? require(`../${serverConfigPath}`) : false;
+
+module.exports = function (gulp, $) {
+
+    // Prevents running the task if no serverConfig file is available
+    gulp.task('ssh:check', function () {
+        if (!serverConfig) {
+            console.error(`ERROR: ${serverConfigPath} missing`);
+            console.error(`To upload the files to the server you need a ${serverConfigPath} file with the server credentials in the project root.`);
+
+            process.exit(1);
+        }
+    });
+
+    if (serverConfig) {
+        const gulpSSH = new GulpSSH({
+            ignoreErrors: false,
+            sshConfig: serverConfig,
+        });
+
+        // Uploads dist files to temporary server folder
+        gulp.task('ssh:upload', function () {
+            return gulp
+                .src(['./dist/**/*', './dist/.*'])
+                .pipe(gulpSSH.dest(`${remotePath}-temp`));
+        });
+
+        // Switches old dist folder from server with new deployment
+        gulp.task('ssh:deploy', ['ssh:upload'], function () {
+            return gulpSSH
+                .shell([`rm -rf ${remotePath}`, `mv ${remotePath}-temp ${remotePath}`]);
+        });
+    }
+
+};
