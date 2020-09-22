1cbae7094411077e55586bbbb8b2291a326627c4
Cristina Coves Mora <cristina.coves-mora@cybercon.de>
2020-09-22 16:11:18 +0200

    #35: adjusted HTML/CSS as well as commercials.json and films.json for clips module enhancement

--- src/patterns/p-teaser/p-teaser-clip.vue
+++ src/patterns/p-teaser/p-teaser-clip.vue
@@ -2,10 +2,14 @@
     <li class="p-teaser-clip"
         :class="addClass">
         <router-link :to="`${$route.path}${clip.route}/`" class="p-teaser-clip__link" @click.native="$root.saveScrollPos">
-            <picture>
-                <source :srcset="clip.image" :media="`(min-width: ${$root.breakpoints.desktop})`">
-                <img class="p-img-cover" :src="clip.imagemobile ? clip.imagemobile : clip.image" alt="">
-            </picture>
+
+
+            <div class="p-teaser-clip__img-wp">
+                <picture v-for="(image, index) in clip.images" :key="index" class="p-teaser-clip__img-item">
+                    <source :srcset="image.url" :media="`(min-width: ${$root.breakpoints.desktop})`">
+                    <img class="p-img-cover" :class="{'u-hidden-mobile': !image.showMobile}" :src="image.urlMobile" alt="">
+                </picture>
+            </div>
             <div class="p-teaser-clip__txt">
                 <h2 class="p-teaser-clip__title"><strong>{{ clip.title }}</strong> {{ clip.additional }}</h2>
                 <p class="p-teaser-clip__descrp" v-if="clip.director">{{ clip.director }}</p>
