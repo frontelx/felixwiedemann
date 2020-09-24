49974df8c958dfc5216e8427cba2d8ef652f5be8
Cristina Coves Mora <cristina.coves-mora@cybercon.de>
2020-09-24 18:02:55 +0200

    #29 Multiple Videos per project page + Video Description Text Format & Space: added adjustments to commercials.json, films.json to add "videos" array, refactored 'm-player.vue' template

--- src/modules/m-player/m-player.vue
+++ src/modules/m-player/m-player.vue
@@ -1,26 +1,24 @@
 <template>
     <div class="m-player p-txt-copy" v-if="Object.keys(clip).length">
         <link rel="preconnect" href="https://player.vimeo.com">
-        <div class="m-player__video">
-            <iframe :src="`https://player.vimeo.com/video/${clip.video}`" frameborder="0"
-                    webkitallowfullscreen mozallowfullscreen allowfullscreen class="m-player__iframe"></iframe>
-        </div>
 
-        <div class="m-player__txt">
-            <h1 class="m-player__title"><strong>{{ clip.title }}</strong> {{ clip.additional }} {{ clip.type }}</h1>
-            <p class="m-player__descrip" v-if="clip.director || clip.producer">{{ clip.director }} {{ clip.producer }}</p>
-            <p class="m-player__descrip" v-if="clip.actors" v-html="clip.actors"></p>
-            <p class="m-player__descrip m-player__descrip--awards" v-if="clip.special" v-html="clip.special"></p>
-        </div>
-
-        <div class="m-player__video2" v-if="clip.video2">
+        <div  v-for="(video, index) in clip.videos">
             <div class="m-player__video">
-                <iframe :src="`https://player.vimeo.com/video/${clip.video2}`" frameborder="0"
+                <iframe :src="`https://player.vimeo.com/video/${video.id}`" frameborder="0"
                         webkitallowfullscreen mozallowfullscreen allowfullscreen class="m-player__iframe"></iframe>
             </div>
 
-            <div class="m-player__txt" v-if="clip.video2title">
-                <p class="m-player__descrip">{{ clip.video2title }}</p>
+            <div class="m-player__txt">
+                <h1 class="m-player__title" v-if="index == 0"><strong>{{ clip.title }}</strong> {{ clip.additional }} {{ clip.type }}</h1>
+                <h2 class="m-player__title" v-else-if="video.title"><strong>{{ video.title }}</strong> {{ video.additional }}</h2>
+                <div class="m-player__title" v-else>&nbsp;</div>
+
+                <!-- show additional description ONLY for first video -->
+                <div v-if="index == 0">
+                    <p class="m-player__descrip" v-if="clip.director || clip.producer">{{ clip.director }} {{ clip.producer }}</p>
+                    <p class="m-player__descrip" v-if="clip.actors" v-html="clip.actors"></p>
+                    <p class="m-player__descrip m-player__descrip--awards" v-if="clip.special" v-html="clip.special"></p>
+                </div>
             </div>
         </div>
 
