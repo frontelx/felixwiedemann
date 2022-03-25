c1f945d7012260459f251a275f169f32f7c37bec
agorilla <agorilla@me.com>
2022-03-25 12:36:57 +0100

    #38: New page for series

---
+++ src/pages/series.vue
@@ -0,0 +1,33 @@
+<template>
+    <main class="main">
+        <h1 v-if="!$route.params.clip" class="visuallyhidden">{{ this.pageConfig.title }}</h1>
+
+        <m-clips :clipsData="'/content/series/series.json'"></m-clips>
+    </main>
+</template>
+
+<script>
+    export default {
+
+        // Component variables
+        data() {
+            return {
+                pageConfig: this.$root.getPageConfigByRoute('/series/', this.$root.navigation),
+            }
+        },
+
+        // Watches url changes
+        watch: {
+            $route (to, from){
+                // Change theme to dark if player or not
+                this.$root.theme = this.$route.params.clip ? 't-dark' : this.pageConfig.theme;
+            }
+        },
+
+        // Runs when component is rendered
+        created() {
+            this.$root.theme = this.pageConfig.theme;
+            this.$root.bgImg = this.pageConfig.bgImg;
+        },
+    }
+</script>
